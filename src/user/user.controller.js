b728e8bdd75e20e99ed54a780f50451189fca1e0
asifgenius <asif15-11288@diu.edu.bd>
2023-11-26 23:49:21 +0600

    Added signup and signin function

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,4 +1,86 @@
-const userService = require('./user.service')
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const { model } = require('mongoose');
+const User = model("User")
+const userService = require('./user.service');
+
+exports.signup = async (req, res) => {
+    const { firstName, lastName, password, email, mobile, address } = req.body;
+    try {
+        const existingUser = await User.findOne({ email: email })
+        if (existingUser) {
+            return res.status(400).json({
+                massage: "User Already Exisiting"
+            })
+        }
+        const hashPassword = await bcrypt.hash(password, 12)
+        const users = await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashPassword,
+            mobile: mobile,
+            address: address
+        })
+        const token = jwt.sign({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashPassword,
+            mobile: mobile,
+            address: address
+        }, process.env.SECRET_KEY)
+        res.status(200).json({
+            user: users,
+            token: token
+        })
+    }
+    catch (error) {
+        console.log("error", error)
+        res.status(400).join({
+            massage: "Error"
+        })
+    }
+}
+
+
+exports.signin = async (req, res) => {
+    const { firstName, lastName, password, email, mobile, address } = req.body;
+    try {
+        const existingUser = await User.findOne({ email: email })
+        if (!existingUser) {
+            return res.status(404).json({
+                massage: "User Not Found"
+            })
+        }
+
+        const matchPassword = await bcrypt.compare(password, existingUser.password)
+
+        if (!matchPassword) {
+            res.status(400).json({
+                massage: "Not found"
+            })
+        }
+        const token = jwt.sign({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: matchPassword,
+            mobile: mobile,
+            address: address
+        }, process.env.SECRET_KEY)
+        res.status(200).json({
+            user: existingUser, token: token
+        })
+    }
+
+    catch (error) {
+        res.status(400).join({
+            massage: error.massage
+        })
+    }
+}
+
 
 exports.addUsers = async (req, res, next) => {
 	try {
